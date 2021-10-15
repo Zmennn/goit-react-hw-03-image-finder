b1d6e2fe820731598c96a4b0faf885901b074de6
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-10-15 22:05:55 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -8,56 +8,45 @@ class ImageGallery extends Component{
   state = {
         dataArray: [],
         page: 1,
-        firstImgId:0,
+        status:"idle"
     }
 
 
   BASE_URL = "https://pixabay.com/api/"
   KEY = "23012527-abace86bcdc7661bfd5472938"
 
-  async componentDidUpdate(prevProps, prevState) {
+   componentDidUpdate(prevProps, prevState) {
         const { BASE_URL, KEY, props, state } = this;
 
-    if (prevProps.searchRequest !== props.searchRequest) {
-        const url = `${BASE_URL}?key=${KEY}&per_page=12&page=${state.page}&q=${props.searchRequest}`;
-        try {
-               const response=await axios.get(url)      
-               this.setState({
-                  dataArray:response.data.hits
-                             })
-            }catch(err){console.log(err)}          
-    } else if (prevState.page !== state.page) {
-        
-        const url = `${BASE_URL}?key=${KEY}&per_page=12&page=${state.page}&q=${props.searchRequest}`;
-        try {
-               const response=await axios.get(url)      
-            this.setState((prevState) => ({
-                dataArray: [...prevState.dataArray, ...response.data.hits],
-                firstImgId: response.data.hits[0] ? response.data.hits[0].id : 0
-            }));
-            if (response.data.hits[0]) {
-                let Element = document.getElementById(response.data.hits[0].id) ;
-                
-                setTimeout(()=>
-                Element.scrollIntoView({ behavior: "smooth" })
-                    , 800)
-                
-            }
-        } catch (err) { console.log(err) };
-        
+       if (prevProps.searchRequest !== props.searchRequest) {
+           const url = `${BASE_URL}?key=${KEY}&per_page=12&page=${state.page}&q=${props.searchRequest}`;
+           this.setState({status:"pending"})
+           axios.get(url)
+               .then((response) => this.setState({
+                   dataArray: response.data.hits,
+                   status:"resolved"
+               }))
+               .catch((err) => this.setState({ status: "reject" }))
+           
+       } else if (prevState.page !== state.page) {
+           this.setState({status:"pending"})
+           const url = `${BASE_URL}?key=${KEY}&per_page=12&page=${state.page}&q=${props.searchRequest}`;
+       
+           axios.get(url)
+               .then((response) =>{ this.setState((prevState) => ({
+                   dataArray: [...prevState.dataArray, ...response.data.hits],
+                   status:"resolved"
+                    })); 
+               
+             if (response.data.hits[0]) {
+                  let Element = document.getElementById(response.data.hits[0].id) ;                
+                  setTimeout(()=>
+                 Element.scrollIntoView({ behavior: "smooth" }), 900)               
+                   }              
+               }).catch((err) => this.setState({ status: "reject" }))
+        }
     }
-    };
-
-
-
-// setTimeout(()=>
-//                 window.scrollTo({
-//                 top: Element.scrollHeight,
-//                  behavior: 'smooth',
-//                 })
-//                     , 800)
-
-
+    
 
     
   handleLoadMore = () => this.setState((prevState) => ({
@@ -73,7 +62,7 @@ class ImageGallery extends Component{
                 dataArray={state.dataArray}
                 />
             </ul>
-            {state.dataArray.length && (<Button
+            {state.status==="resolved" && (<Button
             handleLoadMore={handleLoadMore}
             />)}
             </>
