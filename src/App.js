2abb9e51e85f48a5b0b63c963540a45088f61090
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-10-17 16:14:23 +0300

    1.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import { SearchBar, ImageGallery } from "./components/index";
+import { ImageGallery } from "./components/index";
 import "./App.css";
 import { Component } from "react";
 import { ToastContainer } from "react-toastify";
 
 class App extends Component {
-  state = {
-    searchText: "",
-  };
-
-  handleSubmit = (text) => {
-    this.setState({ searchText: text });
-  };
+  state = {};
 
   render() {
-    const { handleSubmit, state } = this;
     return (
       <>
-        <SearchBar handleSubmit={handleSubmit} />
-
-        <ImageGallery searchRequest={state.searchText} />
-
+        <ImageGallery />
         <ToastContainer />
       </>
     );
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,10 +2,10 @@ import { Component } from "react";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import style from "./style.module.css";
-import { Button, ImageGalleryItem, Modal, request } from "./index";
+import {SearchBar, Button, ImageGalleryItem, Modal, request } from "./index";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
-import PropTypes from "prop-types";
+
 
 class ImageGallery extends Component{
 
@@ -13,88 +13,92 @@ class ImageGallery extends Component{
         dataArray: [],
         page: 1,
         status: "idle",
-      showModal: false,
-        modalUrl:""
+        showModal: false,
+        modalUrl: "",
+        searchRequest:""
     }
 
+  getSnapshotBeforeUpdate(prevProps, prevState) { return document.querySelector("body").clientHeight }
+  // 
 
- 
+   componentDidUpdate( a,prevState,pos) {
+        const { state } = this;
 
-   componentDidUpdate(prevProps, prevState) {
-        const {props, state } = this;
-
-     if (prevProps.searchRequest !== props.searchRequest) {
+     if (prevState.searchRequest !== state.searchRequest) {
            this.setState({status:"pending"})
-           
-       request(state.page, props.searchRequest)
+          
+       request(state.page, state.searchRequest)
               .then((res) => {
                if (res.data.hits.length < 1) {
                  toast.warn("Check your request, no data received");
                  this.setState({ status: "idle" });
-                }else{this.setState({
-                   dataArray: res.data.hits,
-                   status:"resolved"
-               })};   
-              })
-               .catch((err) => this.setState({ status: "reject" }))
+               }else{this.setState({
+                  dataArray: res.data.hits,
+                  status:"resolved"
+                    })};})
+               .catch(() => this.setState({ status: "reject" }))
            
        } else if (prevState.page !== state.page) {
            this.setState({status:"pending"})
            
-           request(state.page, props.searchRequest)
+           request(state.page, state.searchRequest)
           .then((res) => {
            if (res.data.hits.length < 1) {
              toast.warn("These are all pictures for this request");
              this.setState({status:"idle"})
-           } else {
+          } else {
              this.setState((prevState) => ({
-                   dataArray: [...prevState.dataArray, ...res.data.hits],
-                   status:"resolved"
-                    })); 
-               
-             if (res.data.hits[0]) {
-                  let Element = document.getElementById(res.data.hits[0].id) ;                
-                  setTimeout(()=>
-                 Element.scrollIntoView({ behavior: "smooth" }), 900)               
-                   }
-           }
-         }
-         )
-               .catch((err) => this.setState({ status: "reject" }))
-        }
-  }
-  
+             dataArray: [...prevState.dataArray, ...res.data.hits],
+             status:"resolved"
+                })); 
+                    
+             window.scrollTo({
+                top: pos-35,
+                behavior: "smooth"
+                     });            
+                }})
+             .catch((err) => {
+               this.setState({ status: "reject" });
+               console.log(err)
+             })      
+    }
+  } 
 
   toggleModal = () => {
     this.setState((prev) => ({
-      showModal: !prev.showModal,
-      status:!prev.showModal?"idle":"resolved"
-    }))
-  }
+    showModal: !prev.showModal,
+    status:!prev.showModal?"idle":"resolved"
+       }))}
   
   
   toggleModalData = (e) => {    
-    this.setState((prev) => ({
+      this.setState((prev) => ({
       showModal: !prev.showModal,
       modalUrl: this.gettingLink(prev,e),
       status:!prev.showModal?"idle":"resolved"       
-        }))
-    }
+        }))}
   
   gettingLink(prev,e) {
     const modalId=e.target.id;
     const obj = prev.dataArray.find((el) => el.id.toString() === modalId)
-    if(obj){return obj.largeImageURL}
-    
-  };
+    if(obj){return obj.largeImageURL}   
+       };
 
   handleLoadMore = () => this.setState((prevState) => ({
-        page: prevState.page + 1
-    }));
+    page: prevState.page + 1
+      }));
+  
+
+  handleSubmit = (text) => {
+    this.setState({ page: 1,searchRequest: text });
+       };
+
+
 
 
   render() {
-    const { state, handleLoadMore, toggleModal, toggleModalData } = this
+    const { state, handleLoadMore, toggleModal, toggleModalData, handleSubmit } = this
+    
     return <>
       {state.status === "reject" && toast.error("unknown error, check connection")}
 
@@ -102,33 +106,31 @@ class ImageGallery extends Component{
         <div  className={style.spinContainer}>
             <Loader type="Circles" color="#00BFFF" height={120} width={120} />
         </div>)
-  }
+           }
 
-        {state.showModal && <Modal
-            toggleModal={toggleModal}>
-              <img src={state.modalUrl} className={style.modalImg} alt="" />
-              
-          </Modal>}
+      {state.showModal &&
+        <Modal
+          toggleModal={toggleModal}>
+          <img src={state.modalUrl} className={style.modalImg} alt="" />             
+        </Modal>}
           
-          {!state.showModal &&
-            <ul className={style.gallery}>
-                <ImageGalleryItem
-                  dataArray={state.dataArray}
-                  toggleModalData={toggleModalData}
-                />
-            </ul>}
-          
-            {state.status==="resolved" && (<Button
-            handleLoadMore={handleLoadMore}
-            />)}
-            </>
+      <SearchBar handleSubmit={handleSubmit} />
+
+      {!state.showModal &&
+        <ul className={style.gallery}>
+            <ImageGalleryItem
+            dataArray={state.dataArray}
+            toggleModalData={toggleModalData}
+              />
+        </ul>}
+         
+      {state.status === "resolved" &&
+        (<Button
+          handleLoadMore={handleLoadMore} />)}
+      
+        </>
     }
 }
 
 
 export { ImageGallery }
-
-ImageGallery.propTypes = {
-    searchRequest:PropTypes.string.isRequired
-}
-
